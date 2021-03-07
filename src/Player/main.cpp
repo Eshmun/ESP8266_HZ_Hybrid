18b697072fb98a638656840c3b9cc1d33cb678b3
Steven Gerritsen <Steven Gerritsen s.gerritsen@locomogo.com>
2021-03-07 15:32:36 +0100

    Switch to ESP32

--- src/Player/main.cpp
+++ src/Player/main.cpp
@@ -1,21 +1,10 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <Adafruit_NeoPixel.h>
+#include <WiFi.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_ST7789.h>
 
-#define NUM_LEDS 8
-#define DATA_PIN D2
 #define DEBUG 0
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-#define OLED_MOSI   D7 //Connect to D1 on OLED
-#define OLED_CLK    D5 //Connect to D0 on OLED 
-#define OLED_DC     D1 //Connect to DC on OLED
-#define OLED_CS     D8 //Connect to CS on OLED
-#define OLED_RESET  D3 //Connect to RES on OLED
+#define NUM_LEDS 8
 
 enum State_enum {HUMAN, ZOMBIE, COMMAND, IDLE};
 enum Command_enum {NONE, START_GAME, SET_STATE, SET_MIN_RSSI_HZ, SET_MIN_RSSI_ZH, SET_CURRENT_TIME, SET_START_TIME };
@@ -31,12 +20,6 @@ uint32_t timeOffset = 0;
 uint64_t startTime = 0;
 
 char ID[] = "29";
-
-
-Adafruit_NeoPixel pixels(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
-  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 struct scanResults
 {
@@ -57,14 +40,6 @@ void setup()
     // put your setup code here, to run once:
     Serial.begin(115200);
 
-    pixels.begin();
-
-    display.begin(SSD1306_SWITCHCAPVCC);
-    display.clearDisplay();
-    display.display();
-
-    ESP.wdtDisable();
-
     char state_name[9] = "Idle.";
     state_name[6] = ID[0];
     state_name[7] = ID[1];
@@ -79,6 +54,7 @@ void setup()
 void loop()
 {
     scanResults scanresults = scanForNetworks();
+
 
     switch (currentState)
     {
@@ -177,7 +153,7 @@ void loop()
             }
         if (timeOffset > 0 && startTime > 0)
         {
-            if ((double)((system_get_rtc_time()*5.75)/1000000) + timeOffset >= startTime )
+            if (false /* todo: check time*/)
             {
                 nextState = HUMAN;
                 setSSID(HUMAN);
@@ -235,31 +211,31 @@ void rssi_to_leds(int rssi, int min_rssi)
         if (j <= b1)
         {
             //leds[j] = CRGB::Green;
-            pixels.setPixelColor(j, pixels.Color(0, 128, 0));
+            //pixels.setPixelColor(j, pixels.Color(0, 128, 0));
         }
         else if (j > b1 && j <= b2)
         {
             //leds[j] = CRGB::Yellow;
-            pixels.setPixelColor(j, pixels.Color(255, 255, 0));
+            //pixels.setPixelColor(j, pixels.Color(255, 255, 0));
         }
         else if (j > b2 && j <= b3)
         {
             //leds[j] = CRGB::Orange;
-            pixels.setPixelColor(j, pixels.Color(255, 100, 0));
+            //pixels.setPixelColor(j, pixels.Color(255, 100, 0));
         }
         else
         {
             //leds[j] = CRGB::Red;
-            pixels.setPixelColor(j, pixels.Color(255, 0, 0));
+            //pixels.setPixelColor(j, pixels.Color(255, 0, 0));
         }
     }
     for (int j = RSSI_LEDS + 1; j <= NUM_LEDS; j++)
     {
         //leds[j] = CRGB::Black;
-        pixels.setPixelColor(j, pixels.Color(0, 0, 0));
+        //pixels.setPixelColor(j, pixels.Color(0, 0, 0));
     }
     //FastLED.show();
-    pixels.show();
+    //pixels.show();
 }
 
 scanResults scanForNetworks()
@@ -356,28 +332,5 @@ void setSSID(int state)
 
 void updateDisplay(int state)
 {
-    display.setTextSize(1);      // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE); // Draw white text
-    display.setCursor(0, 0);     // Start at top-left corner
-    display.cp437(true);         // Use full 256 char 'Code Page 437' font
-    display.clearDisplay();
-    
-    switch (state)
-    {
-    case HUMAN:
-        display.println("HUMAN");
-        break;
-    case ZOMBIE:
-        display.println("ZOMBIE");
-        break;
-    case COMMAND:
-        display.println("COMMAND");
-        break;
-    case IDLE:
-        display.println("IDLE");
-        break;
-    }
-    display.setCursor(0,20);
-    display.println((double)((system_get_rtc_time()*5.75)/1000000));
-    display.display();
+
 }
